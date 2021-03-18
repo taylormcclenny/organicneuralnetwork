a7ff393b99b1f854404ccf8907f6f620d80aae48
Taylor McClenny <Taylor@Taylors-MacBook-Pro-2.local>
2021-03-18 08:11:44 -0400

    train_generations.py now tests and mutates conttinuously

---
+++ mutate_generations.py
@@ -0,0 +1,96 @@
+
+
+import os
+import ast
+import copy
+from random import randrange
+
+
+
+# SYNAPTIC_MUTATION_VALUES = [0.25, 0.125, 0.01]
+SYNAPTIC_MUTATION_VALUES = [0.125, 0.06, 0.01]
+NUM_CHILDREN_PER_SMV_PER_PARENT = 10                # ex. 10 children * 3 smv * 10 parents = 300 new children
+NUM_OF_MUTATIONS = int(816*0.05)       # 816 Synapses from Occ. to SSNs (ex. 816*0.05 = 5%)
+
+''' Get the Highest Generation Number '''
+gen_reports_list = []
+for filename in os.listdir("gen_reports/"):
+    if filename.startswith('gen_'):
+        gen_reports_list.append(filename)
+
+gen_number_list = []
+for gen_report in gen_reports_list:
+
+    gen_number = gen_report.split('gen_')[1].split('_report')[0]
+    if gen_number is not '':
+        gen_number_list.append(gen_number)
+    unique_gen_number_set = set(gen_number_list)
+    ordered_gen_number_list = sorted(unique_gen_number_set)
+    
+highest_gen_number_from_gen_reports = ordered_gen_number_list.pop()
+
+GENERATION_NUMBER = int(highest_gen_number_from_gen_reports)
+NEW_GENERATION_NUMBER = GENERATION_NUMBER + 1
+
+
+
+''' Get the Gen Report with the Highest Generation Number '''
+with open(f"gen_reports/gen_{GENERATION_NUMBER}_report.json", "r") as file:
+    top_10_mutations = file.read()
+# Convert to Python Dictionary
+top_10_mutations = ast.literal_eval(top_10_mutations)
+
+top_10_mutations_list = []
+for mutation in top_10_mutations:
+    top_10_mutations_list.append(mutation)
+# print(top_10_mutations_list)
+
+
+
+''' Create Childern: Per Parent > Per Synaptic Mutation Value > # of Children >> Repeat '''
+
+mutation_count = 1
+for mutation_filename in top_10_mutations:
+
+    ''' Load the Parent Neural Network '''
+    with open(f"mutations/top_10s/{mutation_filename}", "r") as file:
+        starting_post_synaptic_neighbors_dictionary = file.read()
+    # Convert to Python Dictionary
+    onn_map = ast.literal_eval(starting_post_synaptic_neighbors_dictionary)
+
+
+    for synaptic_mutation_value in SYNAPTIC_MUTATION_VALUES:
+
+
+        for _ in range(NUM_CHILDREN_PER_SMV_PER_PARENT):
+
+            ''' Create an deep copy of the Parent NN '''
+            onn_map_copy = copy.deepcopy(onn_map)
+
+            mutation_list = []
+            for _ in range(NUM_OF_MUTATIONS):
+                
+                neuron = str(randrange(1,66))     # random number b/t 1 & 66 (does not include 66)
+                neuron = neuron.zfill(3)
+                mutation_list.append(f'oc_{neuron}')                    # currently limited to Occ->Shape Specific Neurons
+
+            # print(mutation_list)
+
+
+            for neuron_to_mutate in mutation_list:
+
+                synapse_index = randrange(12)    # random number b/t 0 & 12 (does not include 12)
+
+                # subtract SYNAPSE_MUTATION_VALUE from synapse value
+                mutated_synapse_value = round((onn_map_copy[neuron_to_mutate][1][synapse_index] - synaptic_mutation_value), 5)
+                onn_map_copy[neuron_to_mutate][1][synapse_index] = mutated_synapse_value
+
+                # print(f'MUTATION: {neuron_to_mutate}, {onn_map_copy[neuron_to_mutate][1][synapse_index]}')
+
+            name_of_file = f'onn_map_gen_{NEW_GENERATION_NUMBER}_mutation_{mutation_count}_smv_{synaptic_mutation_value}'
+            f = open(f"mutations/{name_of_file}.json", "x")
+            f.write(str(onn_map_copy))
+            f.close()
+
+            onn_map_copy.clear()
+            mutation_count+=1
--- train_generations.py
+++ train_generations.py
@@ -2,22 +2,30 @@
 
 import os
 import time
+from datetime import datetime
 import ast
+import shutil
+import copy
+from random import randrange
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from matplotlib.collections import PatchCollection
 from PIL import Image
 import concurrent.futures
 
-NUM_OF_GENERATIONS = 2
+NUM_OF_GENERATIONS_TO_RUN = 2
 SCORE_MINIMUM = 500
+
+# SYNAPTIC_MUTATION_VALUES = [0.25, 0.125, 0.01]
+SYNAPTIC_MUTATION_VALUES = [0.125, 0.06, 0.01]
+NUM_CHILDREN_PER_SMV_PER_PARENT = 10                # ex. 10 children * 3 smv * 10 parents = 300 new children
+NUM_OF_MUTATIONS = int(816*0.05)       # 816 Synapses from Occ. to SSNs (ex. 816*0.05 = 5%)
 
 SHOW_VF_IMAGE = 'OFF'
 PAUSE_TIME = 0.25
 
 ACTIVATION_THRESHOLD = 0      # For now, all neurons share this AT. Ultimately it should be neuron-specific & self-tuned
-SLEEP_TIME = 0.00
-
+SLEEP_TIME = 0.001
 
 
 ''' Helper Dictionaries & Lists '''
@@ -87,18 +95,105 @@ dd_signal_dictionary = {}
 
 def main():
 
-    GEN_NUM = 0
-    for _ in range(NUM_OF_GENERATIONS):
+    for _ in range(NUM_OF_GENERATIONS_TO_RUN):
 
-        GEN_NUM+=1
+        ''' MUTATION HANDLERS: CREATORS '''
 
-        ''' Mutation Handlers '''
+        ''' Get the Highest Generation Number '''
+        gen_reports_list = []
+        for filename in os.listdir("gen_reports/"):
+            if filename.startswith('gen_'):
+                gen_reports_list.append(filename)
+
+        gen_number_list = []
+        for gen_report in gen_reports_list:
+
+            gen_number = gen_report.split('gen_')[1].split('_report')[0]
+            if gen_number is not '':
+                gen_number_list.append(gen_number)
+            unique_gen_number_set = set(gen_number_list)
+            ordered_gen_number_list = sorted(unique_gen_number_set)
+            
+        highest_gen_number_from_gen_reports = ordered_gen_number_list.pop()
+
+        GENERATION_NUMBER = int(highest_gen_number_from_gen_reports)
+        NEW_GENERATION_NUMBER = GENERATION_NUMBER + 1
+
+
+        ''' Get the Gen Report with the Highest Generation Number '''
+        with open(f"gen_reports/gen_{GENERATION_NUMBER}_report.json", "r") as file:
+            top_10_mutations = file.read()
+        # Convert to Python Dictionary
+        top_10_mutations = ast.literal_eval(top_10_mutations)
+
+        top_10_mutations_list = []
+        for mutation in top_10_mutations:
+            top_10_mutations_list.append(mutation)
+        # print(top_10_mutations_list)
+
+        ''' Create Childern: Per Parent > Per Synaptic Mutation Value > # of Children >> Repeat '''
+
+        mutation_count = 1
+        for mutation_filename in top_10_mutations:
+
+            ''' Load the Parent Neural Network '''
+            with open(f"mutations/top_10s/{mutation_filename}", "r") as file:
+                starting_post_synaptic_dictionary_for_mutation = file.read()
+            # Convert to Python Dictionary
+            onn_map = ast.literal_eval(starting_post_synaptic_dictionary_for_mutation)
+
+
+            for synaptic_mutation_value in SYNAPTIC_MUTATION_VALUES:
+
+
+                for _ in range(NUM_CHILDREN_PER_SMV_PER_PARENT):
+
+                    ''' Create an deep copy of the Parent NN '''
+                    onn_map_copy = copy.deepcopy(onn_map)
+
+                    mutation_list = []
+                    for _ in range(NUM_OF_MUTATIONS):
+                        
+                        neuron = str(randrange(1,66))     # random number b/t 1 & 66 (does not include 66)
+                        neuron = neuron.zfill(3)
+                        mutation_list.append(f'oc_{neuron}')                    # currently limited to Occ->Shape Specific Neurons
+
+                    # print(mutation_list)
+
+
+                    for neuron_to_mutate in mutation_list:
+
+                        synapse_index = randrange(12)    # random number b/t 0 & 12 (does not include 12)
+
+                        # subtract SYNAPSE_MUTATION_VALUE from synapse value
+                        mutated_synapse_value = round((onn_map_copy[neuron_to_mutate][1][synapse_index] - synaptic_mutation_value), 5)
+                        onn_map_copy[neuron_to_mutate][1][synapse_index] = mutated_synapse_value
+
+                        # print(f'MUTATION: {neuron_to_mutate}, {onn_map_copy[neuron_to_mutate][1][synapse_index]}')
+
+                    name_of_file = f'onn_map_gen_{NEW_GENERATION_NUMBER}_mutation_{mutation_count}_smv_{synaptic_mutation_value}'
+                    f = open(f"mutations/{name_of_file}.json", "x")
+                    f.write(str(onn_map_copy))
+                    f.close()
+
+                    onn_map_copy.clear()
+                    mutation_count+=1
+
+
+
+        ''' Mutation Handlers: Loaders '''
 
         # Go to 'mutations' folder and get all mutation files
         mutation_list = []
         for filename in os.listdir("mutations"):
             if filename.startswith('onn_'):
                 mutation_list.append(filename)
+
+        # Set the GENERATION_NUMBER from the "gen_" of the file
+        last_mutation_in_file_list = mutation_list[-1]
+        gen_num = last_mutation_in_file_list.split('gen_')[1].split('_mutation')[0]
+        if gen_num is not '':
+            GENERATION_NUMBER = gen_num
 
         # Use all mutation files to create list of Synapse Mutation Values ("SMV" or "smv")
         smv_list = []
@@ -134,7 +229,7 @@ def main():
         print(mutations_ordered_by_smv)
 
 
-        ''' Begin: Mutation > Shape > Starting Position >> Repeat '''
+        ''' Begin: Mutation > Shape (x3 ) > Starting Position (x8) >> Repeat '''
 
         mutation_run_count = 1
         mutation_performance = {}
@@ -657,44 +752,86 @@ def main():
 
         mutation_fitness = {}
         top_10_mutations = {}
-        top_10_score_list = [0]
+        top_10_score_list = [SCORE_MINIMUM]
+
+        
         for mutation in mutation_performance:
 
-            all_predictions_list = mutation_performance[mutation]
-
+            # Aggregate all Predictions & Certainties to create 1 Score per Mutation
             score = 0
+            all_predictions_list = mutation_performance[mutation]
             for prediction in all_predictions_list:
-
                 if prediction[0] == prediction[1]:
-
                     prediction_certainty = prediction[2]
-                    score = score + 100 + (prediction_certainty - 33.333)
-
-                else:
-                    pass
+                    score = round((score + 100 + (prediction_certainty - 33.333)), 6)
 
             if score >= SCORE_MINIMUM:
+                mutation_fitness[mutation] = score
 
-                mutation_fitness[mutation] = round(score, 5)
+                # If Top 10 Score, add to 'top_10_mutations'
+                if score > top_10_score_list[-1]:
 
-                if any(top_score < score for top_score in top_10_score_list):
+                    # We'll add a few too many mutations.. but trim back down to 10 later
                     top_10_score_list.append(score)
+                    top_10_mutations[mutation] = score
                     top_10_score_list = sorted(top_10_score_list)
                     top_10_score_list.reverse()
-
                     if len(top_10_score_list) > 10:
                         top_10_score_list.pop()
 
-                    if score in top_10_score_list:
-                        top_10_mutations[mutation] = score
+        # Trim Top 10 Mutations Scores down to 10
+        lowest_top_10 = top_10_score_list[-1]
+        list_of_mutations_to_delete = []
+        for mutation in mutation_fitness:
+            score = mutation_fitness[mutation]
+            if score < lowest_top_10:
+                list_of_mutations_to_delete.append(mutation)
+
+
+        # print("top_10_mutations", top_10_mutations)
+        # print("list_of_mutations_to_delete", list_of_mutations_to_delete)
+
+        for mutation in list_of_mutations_to_delete:
+            top_10_mutations.pop(mutation, None)
+
+        # print(f'MUTATION FITNESS: {mutation_fitness}')
+        # print("TOP 10 MUTATIONS:", top_10_mutations)
+
+        # now = datetime.now()
+
+        ''' Create Generation Report - Top 10 Mutations in Generation & Their Scores '''
+        # name_of_file = f'gen_{GENERATION_NUMBER}_report_{now}'
+        name_of_file = f'gen_{GENERATION_NUMBER}_report'
+        try:
+            f = open(f"gen_reports/{name_of_file}.json", "x")
+            f.write(str(top_10_mutations))
+            f.close()
+        except FileExistsError:
+            print(f'{name_of_file} - File already exist')
+
+
+        ''' Move Top 10 Mutations to '/mutations/top_10s/' '''
+        for top_10_mutation in top_10_mutations:
+            shutil.move(f'mutations/{top_10_mutation}', f'mutations/top_10s/{top_10_mutation}')
+
+        mutations_to_delete_list = []
+        for filename in os.listdir("mutations/"):
+            if filename.startswith('onn_'):
+                mutations_to_delete_list.append(filename)
+
+        print("mutations_to_delete_list", mutations_to_delete_list)
         
+        for mutation_to_delete in mutations_to_delete_list:
+            file_path = f'mutations/{mutation_to_delete}'
+            try:
+                os.remove(file_path)
+            except FileNotFoundError:
+                print(f'{file_path} does not exist')
 
-        print("TOP 10 MUTATIONS", top_10_mutations)
 
-        name_of_file = f'gen_{GEN_NUM}_report'
-        f = open(f"gen_reports/{name_of_file}.json", "x")
-        f.write(str(top_10_mutations))
-        f.close()
+
+
+
 
 
 
